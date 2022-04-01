5fc94e9392d936b49a70f4d31d7c7728d730f77a
sharkeeyy <sfmei2003@yandex.ru>
2022-04-01 17:09:03 +0300

    - learn.javascript.ru (Arrays)

--- Training/Testing scripts/test.js
+++ Training/Testing scripts/test.js
@@ -1,4 +1,25 @@
-let x = 7 / 2;
+function camelize(string) {
+    arr = string.split('-');
 
-console.log(x)
+    arr = arr.map((item, index) => {
+        return index == 0 ? item : item[0].toUpperCase() + item.slice(1);
+    }); 
 
+    return arr.join('');
+
+}
+
+// function camelize(str) {
+//     return str
+//       .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+//       .map(
+//         // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+//         // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+//         (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+//       )
+//       .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+//   }
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
