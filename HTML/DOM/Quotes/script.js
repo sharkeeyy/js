5384c03327453a297965ecde32052c430da6a044
sharkeeyy <sfmei2003@yandex.ru>
2022-07-21 11:49:36 +0300

    zZZ

--- HTML/DOM/Quotes/script.js
+++ HTML/DOM/Quotes/script.js
@@ -2,24 +2,17 @@ let btn = document.querySelector("#new_quote");
 let quote = document.querySelector(".quote");
 let person = document.querySelector(".person");
 
-const quotes = [
-    {quote: 'Quote1', person: 'Person1'},
-    {quote: 'Quote2', person: 'Person2'},
-    {quote: 'Quote3', person: 'Person3'},
-    {quote: 'Quote4', person: 'Person4'},
-    {quote: 'Quote5', person: 'Person5'},
-];
-
 btn.addEventListener('click', getQuote);
 
-function getQuote() {
-    let request = new XMLHttpRequest();
-    request.open('GET', "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote");
-    request.responseType = 'text';
-    
-    request.onload = function() {
-        console.log(request.response);
-    };
+async function getQuote() {
 
-    request.send();
+    let url = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en';
+    let response = await fetch(url);
+
+    let data = await response.json(); 
+
+    console.log(data);
+
+    quote.innerText = data.text;
+
 }
