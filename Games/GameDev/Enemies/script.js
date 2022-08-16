1713519ef3352922e5378388bf8b4f7379925889
sharkeeyy <sfmei2003@yandex.ru>
2022-08-16 11:27:31 +0300

    added Enemies

---
+++ Games/GameDev/Enemies/script.js
@@ -0,0 +1,129 @@
+/**  @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width = 400;
+const CANVAS_HEIGHT = canvas.height = 700;
+
+let gameFrame = 0;
+const ENEMIES_COUNT = 20;
+const enemiesArray = [];
+
+class Enemy {
+    constructor() {
+        this.image = new Image();
+        this.image.src = './img/enemy1.png';
+        this.x = Math.random() * 400;
+        this.y = Math.random() * 700;
+        this.spriteWidth = 293;
+        this.spriteHeigth = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeigth / 2.5;
+        this.speed = Math.random() * 4 - 2;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    }
+
+    update() {
+        this.x += this.speed;
+        this.y += this.speed;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeigth, 
+            this.x, this.y, this.width, this.height);
+        
+    }
+}
+
+class Enemy2 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = './img/enemy2.png';
+        this.x = Math.random() * 400;
+        this.y = Math.random() * 700;
+        this.spriteWidth = 266;
+        this.spriteHeigth = 188;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeigth / 2.5;
+        this.speed = Math.random() * 4 + 1;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = Math.random() * 2;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
+    }
+
+    update() {
+        this.x -= this.speed;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeigth, 
+            this.x, this.y, this.width, this.height);
+        
+    }
+}
+
+class Enemy3 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = './img/enemy3.png';
+        this.x = Math.random() * 400;
+        this.y = Math.random() * 700;
+        this.spriteWidth = 218;
+        this.spriteHeigth = 177;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeigth / 2.5;
+        this.speed = Math.random() * 4 + 1;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = Math.random() * 2;
+        this.angleSpeed = Math.random() * 2 + 0.5;
+        this.curve = Math.random() * 200 + 50;
+    }
+
+    update() {
+        this.x = this.curve * Math.sin(this.angle * Math.PI / 180) - this.width / 2 + CANVAS_WIDTH / 2;
+        this.y = this.curve * Math.cos(this.angle * Math.PI / 180) - this.height / 2 + CANVAS_HEIGHT / 2;
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeigth, 
+            this.x, this.y, this.width, this.height);
+        
+    }
+}
+
+for (i = 0; i < ENEMIES_COUNT; i++) {
+    enemiesArray.push(new Enemy3());
+}
+
+function animate() {
+    ctx.clearRect(0, 0 , CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
+
