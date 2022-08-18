e7adb21dbbdd982417fe1a617709ca2e2b78b402
sharkeeyy <sfmei2003@yandex.ru>
2022-08-18 17:20:12 +0300

    enemy 3 done

--- Games/GameDev/Enemies/script.js
+++ Games/GameDev/Enemies/script.js
@@ -3,11 +3,11 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext("2d");
 
-const CANVAS_WIDTH = canvas.width = 400;
+const CANVAS_WIDTH = canvas.width = 700;
 const CANVAS_HEIGHT = canvas.height = 700;
 
 let gameFrame = 0;
-const ENEMIES_COUNT = 20;
+const ENEMIES_COUNT = 50;
 const enemiesArray = [];
 
 class Enemy {
@@ -79,7 +79,7 @@ class Enemy3 {
     constructor() {
         this.image = new Image();
         this.image.src = './img/enemy3.png';
-        this.x = Math.random() * 400;
+        this.x = Math.random() * 700;
         this.y = Math.random() * 700;
         this.spriteWidth = 218;
         this.spriteHeigth = 177;
@@ -88,14 +88,49 @@ class Enemy3 {
         this.speed = Math.random() * 4 + 1;
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = Math.random() * 2;
-        this.angleSpeed = Math.random() * 2 + 0.5;
-        this.curve = Math.random() * 200 + 50;
+        this.angle = Math.random() * 500;
+        this.angleSpeed = Math.random() * 1 + 0.5;
+        this.curve = Math.random() * 300 + 50;
     }
 
     update() {
-        this.x = this.curve * Math.sin(this.angle * Math.PI / 180) - this.width / 2 + CANVAS_WIDTH / 2;
-        this.y = this.curve * Math.cos(this.angle * Math.PI / 180) - this.height / 2 + CANVAS_HEIGHT / 2;
+        this.x = CANVAS_WIDTH / 2 * Math.sin(this.angle * Math.PI / 90) - this.width / 2 + CANVAS_WIDTH / 2;
+        this.y = CANVAS_HEIGHT / 2 * Math.cos(this.angle * Math.PI / 180) - this.height / 2 + CANVAS_HEIGHT / 2;
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeigth, 
+            this.x, this.y, this.width, this.height);
+        
+    }
+}
+
+class Enemy4 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = './img/enemy3.png';
+        this.x = Math.random() * 700;
+        this.y = Math.random() * 700;
+        this.spriteWidth = 218;
+        this.spriteHeigth = 177;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeigth / 2.5;
+        this.speed = Math.random() * 4 + 1;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = Math.random() * 500;
+        this.angleSpeed = Math.random() * 1 + 0.5;
+        this.curve = Math.random() * 300 + 50;
+    }
+
+    update() {
+        this.x = CANVAS_WIDTH / 2 * Math.sin(this.angle * Math.PI / 90) - this.width / 2 + CANVAS_WIDTH / 2;
+        this.y = CANVAS_HEIGHT / 2 * Math.cos(this.angle * Math.PI / 180) - this.height / 2 + CANVAS_HEIGHT / 2;
         this.angle += this.angleSpeed;
         if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
         if (gameFrame % this.flapSpeed === 0) {
@@ -111,7 +146,7 @@ class Enemy3 {
 }
 
 for (i = 0; i < ENEMIES_COUNT; i++) {
-    enemiesArray.push(new Enemy3());
+    enemiesArray.push(new Enemy4());
 }
 
 function animate() {
