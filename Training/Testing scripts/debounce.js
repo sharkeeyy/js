bbf4eac7c23f357004a188f37b4f2c9365ee77a5
sharkeeyy <sfmei2003@yandex.ru>
2022-12-27 13:41:47 +0300

    solved debounce task

---
+++ Training/Testing scripts/debounce.js
@@ -0,0 +1,24 @@
+const f = debounce(console.log, 500);
+
+
+setTimeout(() => f(1), 0);
+setTimeout(() => f(2), 0); 
+setTimeout(() => f(3), 0);
+setTimeout(() => f(4), 0);
+setTimeout(() => f(5), 0); 
+setTimeout(() => f(6), 600);
+setTimeout(() => f(7), 700);
+setTimeout(() => f(8), 800);
+setTimeout(() => f(9), 900);
+setTimeout(() => f(10), 1000); 
+
+
+function debounce(callback, delay) {
+  let timer;
+  return function (...args) {
+     clearTimeout(timer);
+     timer = setTimeout(() => {
+      callback.apply(this, args);
+     }, delay);
+  }
+}
