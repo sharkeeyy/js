7e3b44efb67f7e166f4a50548ea7e967db8e46f7
sharkeeyy <sfmei2003@yandex.ru>
2023-04-27 22:38:38 +0300

    implemented non-recursive tree BFS

--- Training/Testing scripts/goTreeStraight.js
+++ Training/Testing scripts/goTreeStraight.js
@@ -24,25 +24,24 @@ function goTreeStraigth(tree) {
 
 function goTreeStraigthNonRecursive(tree) {
     let result = [];
+    let stack = [];
 
-    function getNextNode(node) {
-        result.push(node.value);
-        
-        if (node.nodeList === null) return;
-        
-        for (let child of node.nodeList) {
-            getNextNode(child);
-        } 
+    stack.push(tree);
+    while (stack.length !== 0) {
+        let current = stack.pop();
+        result.push(current.value);
+        if (current.nodeList) {
+            stack.push(...current.nodeList.reverse());
+        }
     }
 
-    getNextNode(tree);
     return result;
 }
 
 let tree = new Node(1, [
     new Node(2, [new Node(1, null), new Node(1, null)]),
     new Node(3, [new Node(1, null)]),
-    new Node(4, [new Node(1, null), new Node(1, null), new Node(1, null)]),
+    new Node(4, [new Node(1, null), new Node(2, null), new Node(1, null)]),
 ]);
    
-console.log(goTreeStraigth(tree));
+console.log(goTreeStraigthNonRecursive(tree));
