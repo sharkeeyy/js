a940bd4d0ca357aa2849d0dae2a02853bee38b4a
sharkeeyy <sfmei2003@yandex.ru>
2024-06-18 11:23:41 +0300

    promise pool solved

--- LeetCode/Promise-Pool.js
+++ LeetCode/Promise-Pool.js
@@ -30,11 +30,41 @@ var promisePool = async function (functions, n) {
   });
 };
 
-const promisePoolAsync = async function(functions, n) {
+const promisePoolAsync = async function (functions, n) {
   const helper = () => functions[n++]?.().then(helper);
 
-  await Promise.all(functions.slice(0,n).map(fn=> fn().then(helper)));
+  await Promise.all(functions.slice(0, n).map((fn) => fn().then(helper)));
 };
 
+function pool(array, n) {
+  return new Promise((resolve, reject) => {
+    let pending = 0;
+    let i = 0;
+    let max = array.length - 1;
+
+    function callback() {
+      pending++;
+      console.log('pending ', i);
+      let k = i;
+      array[i]().then(() => {
+        console.log('done ', k);
+        pending--;
+        if (i > max && pending === 0) {
+          resolve('End');
+        } else if (i <= max) {
+          callback();
+        } else {
+          return null;
+        }
+      });
+      i++;
+    }
+
+    for (let j = 0; j < n && j <= max; j++) {
+      callback();
+    }
+  });
+}
+
 const sleep = (t) => new Promise((res) => setTimeout(res, t));
-promisePool([() => sleep(1000), () => sleep(1000), () => sleep(1000)], 2).then(console.log);
+pool([() => sleep(1000), () => sleep(1000), () => sleep(1000)], 1).then(console.log);
